41a2da6d30120892b684ec2a93632ac87bffe1c6
MeCatooo <adam.turek@microsoft.wsei.edu.pl>
2021-10-17 13:04:54 +0200

    Add project files.

---
+++ Projekt ASP/Controllers/AchievementController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt_ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.Controllers
+{
+    public class AchievementController : Controller
+    {
+        static List<Achievement> achievements = new List<Achievement>();
+        public IActionResult Index()
+        {
+            return View(achievements);
+        }
+        public IActionResult AddAchievement()
+        {
+            return View();
+        }
+        public IActionResult Add(Achievement achievement)
+        {
+            achievements.Add(achievement);
+            return View("Index", achievements);
+        }
+    }
+}
---
+++ Projekt ASP/Models/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.Models
+{
+    public class Achievement
+    {
+        public string Name { get; set; }
+        public string Game { get; set; }
+        public string Achieved { get; set; }
+
+    }
+}
