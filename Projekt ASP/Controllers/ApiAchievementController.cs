7b797a0a3a43ac0741fa1a6ddcb57b445d5964f0
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-01-12 18:29:40 +0100

    API

--- Projekt ASP/Controllers/ApiAchievementController.cs
+++ Projekt ASP/Controllers/ApiAchievementController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Projekt_ASP.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/achievement")]
     [ApiController]
     public class ApiAchievementController : ControllerBase
     {
@@ -18,7 +18,53 @@ namespace Projekt_ASP.Controllers
         {
             this.repository = repository;
         }
+        [HttpDelete]
+        [Route("{id?}")]
+        public IActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Delete(id);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
 
+        [HttpGet]
+        public List<Achievement> GetAll()
+        {
+            return repository.FindAll().ToList();
+        }
 
+        [HttpGet]
+        [Route("{id?}")]
+        public IActionResult Get(int id)
+        {
+            var achievement = repository.FindAchievementById(id);
+            if (achievement != null)
+            {
+                return new OkObjectResult(achievement);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost]
+        public IActionResult Add([FromBody] Achievement achievement)
+        {
+            if (ModelState.IsValid)
+            {
+                Achievement achievement1 = repository.Add(achievement);
+                return new CreatedResult($"/api/achievement/{achievement1.Id}", achievement);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
     }
 }
